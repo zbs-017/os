c8f5f1a645d17cc528bd7ef9ad9237a1d4f61281
zbs <1836793231@qq.com>
2023-04-05 13:02:57 +0800

    实模式打印

--- kernel/src/boot.asm
+++ kernel/src/boot.asm
@@ -13,12 +13,26 @@ mov sp, 0x7c00
 
 xchg bx, bx
 
-mov ax, 0xb800
-mov ds, ax
-mov byte [0], 'H'
+mov si, bootingMsg
+call print
 
 ; 阻塞
 jmp $
+
+print:
+    mov ah, 0x0e  ; 0x10 的打印子功能号
+    .next:
+        mov al, [si]
+        cmp al, 0
+        jz .down
+        int 0x10  ; 调用 BOIS 的打印功能
+        inc si
+        jmp .next
+    .down
+        ret 
+
+bootingMsg:
+    db "Booting ...", 13, 10, 0  ; /n/r
 
 ; 填充到 512 个字节
 times 510 - ($ - $$) db 0
